e402ceb7a78896d29ff9e9d62e28cc77d1167d09
Joshua Gray <graydog.8719@gmail.com>
2024-06-07 10:56:21 -0600

    added some query functions to queries.js and fixed some filenames in .json files

--- database/queries.js
+++ database/queries.js
@@ -1,3 +1,65 @@
-const { Saber, Color, Emitter, ColoredEmitter, Guard, Switch, 
-    Pommel, Soundfont, User, Post, Like } = await import ('./model.js')
+const { Saber, 
+    Color, 
+    Emitter, 
+    ColoredEmitter, 
+    Guard, 
+    Switch, 
+    Pommel, 
+    Soundfont, 
+    User, 
+    Post, 
+    Like 
+} = await import ('./model.js')
 
+const queryFunctions = {
+    getColorImage: async (saber) => {
+        const colorImage = await Color.findOne({
+            attributes: [ 'image' ],
+            where: { colorId: saber.colorId }
+        })
+
+        return colorImage
+    },
+    getEmitterImage: async (saber) => {
+        const emitterImage = await Emitter.findOne({
+            attributes: [ 'image' ],
+            where: { emitterId: saber.emitterId }
+        })
+
+        return emitterImage
+    },
+    getColoredEmitterImage: async (saber) => {
+        const coloredEmitterImage = await ColoredEmitter.findOne({
+            attributes: [ 'image' ],
+            where: { coloredEmitterId: saber.coloredEmitterId }
+        })
+
+        return coloredEmitterImage
+    },
+    getGuardImage: async (saber) => {
+        const guardImage = await Guard.findOne({
+            attributes: [ 'image' ],
+            where: { guardId: saber.guardId }
+        })
+
+        return guardImage
+    },
+    getSwitchImage: async (saber) => {
+        const switchImage = await Switch.findOne({
+            attributes: [ 'image' ],
+            where: { switchId: saber.switchId }
+        })
+
+        return switchImage
+    },
+    getPommelImage: async (saber) => {
+        const pommelImage = await Pommel.findOne({
+            attributes: [ 'image' ],
+            where: { pommelId: saber.pommelId }
+        })
+
+        return pommelImage
+    }
+}
+
+export default queryFunctions
